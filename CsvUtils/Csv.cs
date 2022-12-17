e2d7c342bcdd315962ada0a72e3b309e7f326e2e
BrunoLoFaro <lofarobruno@gmail.com>
2022-12-17 14:20:35 -0300

    Initial commit

---
+++ CsvUtils/Csv.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CsvUtils
+{
+    public class Csv
+    {
+        public string ruta;
+        public string[] headers;
+        public List<Fila> filas;
+        public Csv(string ruta)
+        {
+            string[] registro= {""};
+            this.ruta = ruta;
+            this.filas = new List<Fila>();
+            const Int32 BufferSize = 128;
+            try
+            {
+                var fileStream = File.OpenRead(ruta);
+                var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize);
+                string linea;
+                if ((linea = streamReader.ReadLine()) != null)//leo la primer linea con los headers
+                    headers = linea.Split(';');
+                while ((linea = streamReader.ReadLine()) != null)//leo el resto
+                {
+                    filas.Add(new Fila(linea.Split(';')));//parseo los registros de la linea
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error al leer el archivo {ruta}",ex);
+            }
+        }
+    }
+}
---
+++ CsvUtils/Fila.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsvUtils
+{
+    public class Fila
+    {
+        public string[] registros;
+        public Fila(string[] registros)
+        {
+            this.registros = registros;
+        }
+    }
+}
